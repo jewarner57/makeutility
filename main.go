c3bcafc0648109a05af20286d07dfc7507be1f48
Jonathan Warner <jewarner57@gmail.com>
2022-05-04 21:52:19 -0400

    feat: get user repo data from github and cache data

--- main.go
+++ main.go
@@ -1,12 +1,74 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-	"os/exec"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+	"time"
 )
 
+type Repo struct {
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	PushedAt    time.Time `json:"pushed_at"`
+	CloneUrl    string    `json:"clone_url"`
+}
+
 func main() {
-	cmd := exec.Command("tesseract", "./eurotext.png", "-")
-	res, _ := cmd.Output()
-	fmt.Print(string(res))
+	var username string = "jewarner57"
+	responseData := fetchRepoData(username)
+
+	repoList := make([]Repo, 0)
+	if err := json.Unmarshal([]byte(responseData), &repoList); err != nil {
+		log.Fatal(err)
+	}
+
+	sort.Slice(repoList, func(i, j int) bool {
+		return repoList[i].UpdatedAt.After(repoList[j].UpdatedAt)
+	})
+
+	fmt.Println(string(repoList[0].CloneUrl))
+}
+
+func fetchRepoData(username string) []byte {
+	info, err := os.Stat("./cache.json")
+	hoursSinceCacheUpdated := time.Since(info.ModTime()).Hours()
+	if errors.Is(err, os.ErrNotExist) || hoursSinceCacheUpdated > 0.5 {
+		updateCacheData(username)
+	}
+
+	jsonFile, err := os.Open("cache.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cacheData, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return cacheData
+}
+
+func updateCacheData(username string) {
+	response, err := http.Get("https://api.github.com/users/" + username + "/repos")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ioutil.WriteFile("cache.json", []byte(responseData), 0644)
 }
