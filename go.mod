109fe1e7dfa9c06d1549b0cc7dee27a9af9fd10d
Jonathan Warner <jewarner57@gmail.com>
2022-05-05 21:02:36 -0400

    feat: improve output formatting

--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.18
 
 require github.com/fatih/color v1.13.0
 
+require golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
+
 require (
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 )
--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/fatih/color"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 type RepoList struct {
@@ -20,6 +22,7 @@ type RepoList struct {
 
 type Repo struct {
 	Name        string    `json:"name"`
+	GithubUrl   string    `json:"html_url"`
 	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
@@ -44,12 +47,12 @@ func main() {
 	flag.Parse()
 
 	if *sortBy != "" {
-		sortString := "&sort:" + *sortBy
+		sortString := "%20sort:" + *sortBy
 		sortBy = &sortString
 	}
 
 	if *language != "" {
-		languageString := "&language:" + *language
+		languageString := "%20language:" + *language
 		language = &languageString
 	}
 
@@ -65,15 +68,24 @@ func main() {
 }
 
 func printRepoList(repoList RepoList) {
+	width, _, _ := terminal.GetSize(0)
+	fmt.Println(strings.Repeat("-", width))
 	for _, repo := range repoList.Items {
 
-		green := color.New(color.FgGreen)
-		boldGreen := green.Add(color.Bold)
+		boldGreen := color.New(color.FgGreen).Add(color.Bold)
+		underLineCyan := color.New(color.FgBlue).Add(color.Underline)
 
 		boldGreen.Println(repo.Name)
-		fmt.Println(repo.Description)
-		fmt.Println(repo.CloneUrl)
-		fmt.Println("------------------")
+		if repo.Description != "" {
+			fmt.Println(repo.Description)
+		}
+		fmt.Println()
+
+		fmt.Print("View: ")
+		underLineCyan.Println(repo.GithubUrl)
+		fmt.Print("Clone: ")
+		underLineCyan.Println(repo.CloneUrl)
+		fmt.Println(strings.Repeat("-", width))
 	}
 }
 
@@ -81,6 +93,8 @@ func fetchRepoData(username string, q string, sortBy string, language string) []
 	response, err := http.Get(
 		"https://api.github.com/search/repositories?q=" + q + "%20user:" + username + sortBy + language,
 	)
+
+	fmt.Println("https://api.github.com/search/repositories?q=" + q + "%20user:" + username + sortBy + language)
 
 	if err != nil {
 		fmt.Print(err.Error())
